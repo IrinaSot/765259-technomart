af43598a80f8592a14920450382099c2e107ef26
IrinaSot <39626361+IrinaSot@users.noreply.github.com>
2018-07-02 20:09:59 +0700

    Исправлены ошибки в декоративных элементах

--- js/popap.js
+++ js/popap.js
@@ -2,6 +2,8 @@
 var link = document.querySelector(".contacts-btn");
 var popup = document.querySelector(".modal-feedback");
 var close = popup.querySelector(".modal-close");
+
+
 link.addEventListener("click", function (evt) {
 evt.preventDefault();
     popup.classList.add("modal-show");
