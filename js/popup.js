32f12cd868171767bb1d7c05ed5529a96fb31133
IrinaSot <39626361+IrinaSot@users.noreply.github.com>
2018-07-03 17:27:32 +0700

    Исправления модальных окон

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,15 @@
- 
 var link = document.querySelector(".contacts-btn");
 var popup = document.querySelector(".modal-feedback");
 var close = popup.querySelector(".modal-close");
+var login = popup.querySelector("name=name");
 
 
 link.addEventListener("click", function (evt) {
 evt.preventDefault();
     popup.classList.add("modal-show");
+    login.focus();
   });
+
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
